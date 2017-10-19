1620d1e06dcc0dd03e546d2160eb364019818a37
Troldenstein <sampsukka@gmail.com>
2017-10-19 17:19:11 +0000

    Typo fix x 2

--- set_pin.py
+++ set_pin.py
@@ -5,9 +5,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 if int(sys.argv[1]) < 2 or int(sys.argv[1]) > 20:
-    print "Invalid argument, give gpio pin number from 3 to 20: ",str(sys.argv[1]) 
+    print "Invalid argument, give gpio pin number from 2 to 20: ",str(sys.argv[1]) 
 elif int(sys.argv[2]) < 0 or int(sys.argv[2]) > 1:
-    print "Invalid second argument, give 1 or 0 to define the uotput state for pin: ",str(sys.argv[2])
+    print "Invalid second argument, give 1 or 0 to define the output state for pin: ",str(sys.argv[2])
 else:
     print "You gave values: ",str(sys.argv[1]),str(sys.argv[2])
     gp1 = int(sys.argv[1])
